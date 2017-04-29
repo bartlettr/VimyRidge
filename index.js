d91824d4027337206c84fe10d182a8cc61c69622
James Ellwood <jellwood@gmail.com>
2017-04-28 20:35:54 -0400

    Add slider to map.

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
-var width = window.innerWidth,
-    height = window.innerHeight;
+var map =  L.map('map', {zoomControl: false, center: [50.36, 2.79], zoom: 13});
+L.control.zoom({position: 'bottomleft'}).addTo(map);
 
-var svg = d3.select( "#map" )
-    .append( "svg" )
-    .attr( "width", width )
-    .attr( "height", height );
-var g = svg.append( "g" );
-
-var projection = d3.geo.mercator()
-  .scale(400000)
-  .center([2.8, 50.361]) // Center the map on Vimy Ridge battle area
-  .translate([width / 2, height / 2]);
-
-var geoPath = d3.geo.path()
-    .projection(projection);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, ' +
+        '&copy; <a href="https://carto.com/attribution">CARTO</a>'
+}).addTo(map);
 
 var paths = [];
-d3.json('GeoJSON/contours_wgs84_apr25.geojson', function(error, mapData) {
-    paths = mapData.features;
-    displayPath(0);
-});
+var currentTimeCode = 0;
 
-function displayPath(timeCode) {
-    var pathsAtTimeCode = paths.filter(function(path) {
-        return path.properties.TimeCode === timeCode;
+var overlay = L.d3SvgOverlay(function(sel, proj) {
+	var pathsAtTimeCode = paths.filter(function(path) {
+        return path.properties.TimeCode === currentTimeCode;
     });
 
-    g.selectAll("path").remove();
+	var upd = sel.selectAll('path')
+		.data(pathsAtTimeCode)
+		.enter()
+	    .append('path')
+	    .attr('d', proj.pathFromGeojson)
+	    .attr('stroke', getStroke)
+	    .attr('fill', 'none');
 
-    g.selectAll("path")
-        .data(pathsAtTimeCode)
-        .enter()
-        .append("path")
-        .attr("d", geoPath)
-        .attr("fill", "none")
-        .attr("stroke", getStroke)
-        .attr("stroke-width", 2);
-
-    g.selectAll("path")
-        .on("click", onClick)
-        .on("mouseover", mouseOver)
-        .on("mouseout", mouseOut);
-}
+    upd.attr('stroke-width', 3 / proj.scale);
+	upd.on("click", onClick);
+});
 
 function getStroke(data) {
     if (data.properties.ObjectiveL === "Jumping off trench") {
@@ -52,40 +35,26 @@ function getStroke(data) {
     return 'gray';
 }
 
-function onClick(data) {
-    if (data.properties.LineCode) {
-        d3.select("h2").html(data.properties.Quote + " (Source: <a href='" +  data.properties.TimeSour_1
-            + "'target='_blank'>" + data.properties.TimeSource + "</a>)");
-        d3.select(this).style("stroke", "cyan");
-        d3.select(this).style("stroke-width", 3);
-    }
-}
-
-function mouseOver(d) {
-    d3.select(this).style("stroke", "cyan");
-    d3.select(this).style("stroke-width", 3);
-    if (d.properties.LineCode) {
-      d3.select("h2").text(d.properties.BattalionR + " reached " + d.properties.ObjectiveL
-        + " line at " + d.properties.TimeArrive);
-    } else {
-        d3.select("h2").text("Estimated front line at " + d.properties.TimeAMPM);
-    }
-}
-
-function mouseOut(d) {
-    d3.select(this).style("stroke", getStroke);
-    d3.select(this).style("stroke-width", 2);
-}
-
-d3.select("#timeslide").on("input", function() {
-    update(+this.value);
+d3.json("GeoJSON/contours_wgs84_apr25.geojson", function(data) {
+    paths = data.features;
+	map.removeLayer(overlay)
+	overlay.addTo(map);
 });
 
-function update(value) {
-  document.getElementById("range").innerHTML=timeArray[value];
-  inputValue = timeArray[value];
-  d3.selectAll(".incident")
-    .attr("fill", timeMatch);
+function onClick(data) {
+	d3.selectAll('path').attr('stroke', getStroke);
+    d3.select(this).style("stroke", "cyan");
+
+    if (data.properties.LineCode) {
+      d3.select("#info-description").text(data.properties.BattalionR + " reached " + data.properties.ObjectiveL
+        + " line at " + data.properties.TimeArrive);
+
+		d3.select("#info-source").html(data.properties.Quote + " (Source: <a href='" +  data.properties.TimeSour_1
+            + "'target='_blank'>" + data.properties.TimeSource + "</a>)");
+    } else {
+        d3.select("#info-description").text("Estimated front line at " + data.properties.TimeAMPM);
+		d3.select("#info-source").html('');
+    }
 }
 
 var startDate = new Date('1917-04-09T05:30Z');
@@ -94,15 +63,17 @@ var endDate = new Date("1917-04-09T15:00Z");
 function onChronitronChange(chronitronDate) {
     var seconds = (chronitronDate.getTime() - startDate.getTime()) / 1000;
     var minutes = seconds / 60;
-    var timeCode = Math.floor(minutes / 15);
-    displayPath(timeCode);
+    currentTimeCode = Math.floor(minutes / 15);
+
+	map.removeLayer(overlay)
+	overlay.addTo(map);
 }
 
 d3.select("#slider")
     .call(chroniton()
         .domain([startDate, endDate])
         .labelFormat(d3.time.format("%B %dth, %I:%M %p %Z"))
-        .width(500)
+        .width(460)
         .playButton(true)
         .playbackRate(0.5)
         .loop(false)
